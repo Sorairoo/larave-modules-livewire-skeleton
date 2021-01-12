e809684cf90a669fb73e1a4f360c53644b15896a
SorairoGozen <celestecaterino@gmail.com>
2021-01-12 19:50:03 +0100

    first commit

---
+++ Modules/Core/Http/Livewire/Counter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Core\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public function render()
+    {
+        return view('core::livewire.counter');
+    }
+}
